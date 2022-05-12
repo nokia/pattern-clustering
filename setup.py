8bfdd4f2a2becddd7dddcaa9718cc04cab8c0f7f
Marc-Olivier Buob <marc-olivier.buob@nokia-bell-labs.com>
2022-05-13 00:54:53 +0200

    Trying to fix setup.py for github actions

--- setup.py
+++ setup.py
@@ -18,8 +18,8 @@ from setuptools     import setup, find_packages
 from distutils.core import Extension
 from setup_cpp      import find_cpp_sources
 
-headers_dir = "boost"
-sources_dir = "boost"
+headers_dir = "./boost"
+sources_dir = "./boost"
 sources = find_cpp_sources(
     use_pyplusplus=False,
     sources_ext="cpp",
@@ -76,7 +76,7 @@ setup(
             sources=sources,
             extra_compile_args=cxx_flags,
             extra_link_args=cxx_flags,
-            include_dirs=[headers_dir],  # Otherwise, GitHub action build.yml fails
+            include_dirs=[headers_dir],
             libraries=[lib_boost]
         )
     ],
